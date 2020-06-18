76f930ed85ff51e0d5807abdfe9f2b7b31dca90e
eitake0002 <eiki.e.t.d@gmail.com>
2020-06-18 15:44:12 +0900

    add django rest framework api

--- restframework/restframework/urls.py
+++ restframework/restframework/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 from django.urls import path, include
 from django.contrib.auth.models import User
 from rest_framework import serializers, viewsets, routers
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
+import os
+import sys
+
+dir = os.getcwd()
+head, tail = os.path.split(dir)
+sys.path.append(head)
+
+from gcp import image_annotation as ia
 
 # Serializers define the API representation.
+# - Serializer is similar to Form and ModelForm in Django. 
+# - Serializers converts complex data and model into native Python data types. 
+# - This is a replacement of Model in usual django usage. 
+# Official Documentation
+# https://www.django-rest-framework.org/api-guide/serializers/
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
@@ -29,10 +45,23 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+    @action(detail=False)
+    def vision_api(self, request, format=None):
+
+        image_uri = request.query_params["url"]
+
+        client = ia.vision_client()
+        response = ia.annotate_image(vision_client=client, image_uri=image_uri)
+        result = ia.extract_top_confidences(response, num=5, display=False)
+
+        return Response(result)
+
 
 # Routers provide a way of automatically determining the URL conf.
+# # Check url patterns with command-line.
+# $ ./manage.py show_urls sss
 router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router.register(r'users', UserViewSet, basename="users")
 
 
 # Wire up our API using automatic URL routing.
